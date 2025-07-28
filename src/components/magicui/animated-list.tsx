b5d26c225dd5e5c91950970a50613d7a6d1846bf
Juan Carlos Saugar <juank98761@gmail.com>
2025-07-28 10:59:38 +0200

    fix

--- src/components/magicui/animated-list.tsx
+++ src/components/magicui/animated-list.tsx
@@ -18,7 +18,14 @@ export function AnimatedListItem({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <motion.div {...animations} layout className="mx-auto w-full">
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1, originY: 0 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{ type: "spring", stiffness: 350, damping: 40 }}
+      layout
+      className="mx-auto w-full"
+    >
       {children}
     </motion.div>
   );
